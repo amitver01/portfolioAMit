8a445986338ea206a516105ca726e8c8f987ad5b
Amit Verma <amitver6969@gmail.com>
2024-12-28 00:04:00 +0530

    resume updated

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -32,7 +32,7 @@ function Landing() {
             padding="py-2 px-4 md:py-3 md:px-6"
             borderRadius="rounded-lg"
             borderColor="border-white-200"
-            link="https://drive.google.com/file/d/1oL7MGvB8GBWFiDtMkngT1bMn-EG7E-mr/view?usp=sharingn"
+            link="https://drive.google.com/file/d/19EliJkypuaE26_zHGBFqVJH_jfVuBePR/view?usp=sharing"
           />
         </div>
       </div>
